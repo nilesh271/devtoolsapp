7f365dda236e29805100c0208cadb1ee9fee9743
nilesh271 <nilesh271@gmail.com>
2026-03-01 15:57:36 +0530

    feat: initial bolierplate with material-tailwindcss

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(),],
+  resolve: {
+    // ensure only one React copy is used, avoid invalid hook call errors when
+    // dependencies (like material-tailwind) bring their own.
+    dedupe: ['react', 'react-dom'],
+  },
+})
